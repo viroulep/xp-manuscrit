3c0a6da675f00adb28b438588c1cf86b1c7f7b25
Philippe Virouleau <philippe.44@gmail.com>
2018-01-26 15:21:55 +0100

    Better scale to highlight nodes

--- brunch/bandwidth/gen_heatmap.R
+++ brunch/bandwidth/gen_heatmap.R
@@ -16,11 +16,17 @@ df_bandwidth = transform(df_bandwidth, heat = (bandwith_gb))
 
 pdf("heatmap_brunch.pdf", width = 10, height=6)
 myplot=ggplot(df_bandwidth, aes(node_source, node_dest))
+print(df_bandwidth)
+#myplot=myplot+ geom_tile(aes(fill = cut(heat, c(0, 3.5, 3.7))))
 myplot=myplot+ geom_tile(aes(fill = heat), colour="white")
 myplot=myplot+ylab("Noeud cible")
 myplot=myplot+xlab("Noeud source")
 #myplot=myplot+ggtitle("Carte de la bande passante sur brunch (memcpy)")
-myplot=myplot+scale_fill_gradient(name="Bande passante (Go/s)", low = "grey90", high = "steelblue")
+#myplot=myplot+scale_fill_manual(name = "Bande passante (Go/s)",
+                     #values = c("(0,3.5]" = "grey90",
+                                  #"(3.5, 3.7]" = "steelblue4"))
+                     #labels = c("<= 17", "17 < qsec <= 19", "> 19"))
+myplot=myplot+scale_fill_gradient2(name="Bande passante (Go/s)", low="white", mid="green3", high="black", midpoint=3.3, limits=c(2.5, 3.61), breaks=c(2.5, 3.3, 3.6))
                                   #breaks=c(1, 2, 3, 4), labels=c("1.0", "2.0", "3.0", "4.0"))
 #myplot=myplot+theme(plot.title = element_text(hjust = 0.5),
                     #legend.position=c(.9,.4)
